58944a9db411399c8f9ec3f51701a76ad12e2848
Curatola <olavo.curatola@gmail.com>
2018-11-15 14:45:51 -0200

    Repair npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,6 @@ import { TurmasPageModule } from '../pages/turmas/turmas.module';
 import { AuthProvider } from '../providers/auth/auth';
 import { IonicStorageModule } from '@ionic/storage';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-=======
-import { PresencaListasProvider } from '../providers/presenca-listas/presenca-listas';
-import { PresencaAlunosProvider } from '../providers/presenca-alunos/presenca-alunos';
-import { TurmasListaProvider } from '../providers/turmas-lista/turmas-lista';
->>>>>>> 969cd8d60ef0e569aa152dff6bbe494cb18cf828
 
 @NgModule({
   declarations: [
@@ -45,16 +39,8 @@ import { TurmasListaProvider } from '../providers/turmas-lista/turmas-lista';
   providers: [
     StatusBar,
     SplashScreen,
-<<<<<<< HEAD
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider
-=======
-    { provide: ErrorHandler, useClass: IonicErrorHandler },
-    AuthProvider,
-    PresencaListasProvider,
-    PresencaAlunosProvider,
-    TurmasListaProvider
->>>>>>> 969cd8d60ef0e569aa152dff6bbe494cb18cf828
   ]
 })
 export class AppModule {}
---
+++ www/build/1.js
@@ -0,0 +1,87 @@
+webpackJsonp([1],{
+
+/***/ 279:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "NovaListaPageModule", function() { return NovaListaPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(20);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__nova_lista__ = __webpack_require__(282);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var NovaListaPageModule = /** @class */ (function () {
+    function NovaListaPageModule() {
+    }
+    NovaListaPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__nova_lista__["a" /* NovaListaPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__nova_lista__["a" /* NovaListaPage */]),
+            ],
+        })
+    ], NovaListaPageModule);
+    return NovaListaPageModule;
+}());
+
+//# sourceMappingURL=nova-lista.module.js.map
+
+/***/ }),
+
+/***/ 282:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return NovaListaPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(20);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+/**
+ * Generated class for the NovaListaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var NovaListaPage = /** @class */ (function () {
+    function NovaListaPage(navCtrl, navParams) {
+        this.navCtrl = navCtrl;
+        this.navParams = navParams;
+    }
+    NovaListaPage.prototype.ionViewDidLoad = function () {
+        console.log('ionViewDidLoad NovaListaPage');
+    };
+    NovaListaPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-nova-lista',template:/*ion-inline-start:"/home/curatola/GitRepo/ionic_IAm_Here/src/pages/nova-lista/nova-lista.html"*/'<!--\n  Generated template for the NovaListaPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Nova_Lista</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"/home/curatola/GitRepo/ionic_IAm_Here/src/pages/nova-lista/nova-lista.html"*/,
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]])
+    ], NovaListaPage);
+    return NovaListaPage;
+}());
+
+//# sourceMappingURL=nova-lista.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=1.js.map
